984d3edfcb0829f589aece5eb80a344f51e2dd96
fspruce <f.spruce@proton.me>
2025-10-28 13:39:24 +0000

    Complete 012_add_one.py

--- 012_add_one.py
+++ 012_add_one.py
@@ -20,5 +20,7 @@ print(add_one(6))
 
 # `add_one` is a function (little machine) that takes a number as an input
 # parameter, adds one to it, and then returns the result.
-
+#
+# Completed
+# 
 # Now move on to 013_add_two.py
